b736ed786173f7da2f1347b591243cbf2045d254
prabinrijal03 <prabinrijal03@gmail.com>
2023-03-20 20:36:04 +0545

    completed

--- public/js/client.js
+++ public/js/client.js
@@ -10,8 +10,19 @@ const append = (message, position)=>{
     messageElement.classList.add(position);
     messageContainer.append(messageElement);
 }
+form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    const message = messageInput.value;
+    append(`You: ${message}`, 'right');
+    socket.emit('send', message);
+    messageInput.value = '';
+});
+
 const nam = prompt('Enter your name to join');
 socket.emit('new-user-joined', nam);
 socket.on('user-joined', nam =>{
-append(`${nam} joined the chat`, 'left');
+append(`${nam} joined the chat`, 'right');
 });
+socket.on('receive', data =>{
+    append(`${data.name}: ${data.message}`, 'left');
+    });
